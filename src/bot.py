b513a14862d36d97b8bca9914a2251d3766700a8
Nathan <nathanfpedroza@gmail.com>
2019-05-05 23:10:15 -0300

    trader class created

--- src/bot.py
+++ src/bot.py
@@ -4,20 +4,46 @@
 from MetaTrader5 import *
 from time import sleep
 from datetime import datetime
+import telepot
 
-def runtime():
+class trader():
+    
 
-    while True:
-        try:
-            MT5Initialize()
-            MT5WaitForTerminal()
+    def __init__(self, lote_user):
+        self.lote = lote_user
+        self.name = "665972859:AAH1u3Wio7GEUko8R_CPAD6gy7nINHlxPJ0"
+        self.bot = telepot.Bot(name)
+        self.runtime()
 
-            print("Testes:")
-            print(MT5TerminalInfo())
-            print("Versão atual do MT5: ", MT5Version())
+    def getOrder():
+        texto = self.bot.getUpdates(['text'])
+        if 'sell' in texto:
+            return 'sell'
+        else if 'buy' in texto:
+            return 'buy'
+
+    def buy(signal, currency, value):
+        self.bot.sendMessage("Signal no ", signal)
+
+    def sell(signal, currency, value, profit):
+        self.bot.sendMessage("Signal no ", signal)
+
+    def takeProfit(value):
+    
+    # INICIALIZA O BOT E O MQL 5
+    def runtime():
+
+        while True:
+            try:
+                MT5Initialize()
+                MT5WaitForTerminal()
+
+                print("Testes:")
+                print(MT5TerminalInfo())
+                print("Versão atual do MT5: ", MT5Version())
+
             
-        
 
-        except EOFError:
-            print("Erro de execução no bot!")
+            except EOFError:
+                print("Erro de execução no bot!")
 
